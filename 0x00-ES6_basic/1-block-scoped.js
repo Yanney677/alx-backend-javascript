a14b2af9dff9186dd6b6ed1cbccf942ffa5c58b4
Yanney677 <w.yanney1991@gmail.com>
2024-03-09 13:21:49 +0000

    Block Scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+/* eslinit-disable no-unused-vars */
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    // eslint
+    const task = true;
+    // eslint
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
