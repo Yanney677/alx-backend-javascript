61a35be16e8d2f29f9c9cc7db8777729a607bd78
Yanney677 <w.yanney1991@gmail.com>
2024-03-13 04:33:38 +0000

    Array

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students.filter(student => student.location === city)
     .map(student => {
-      const studgrade = newGrades.find(grade => grade.studentId === student.id);
-      return {...student, grade: studgrade ? studgrade.grade : 'N/A'};
+      const sgrade = newGrades.find(grade => grade.studentId === student.id);
+      return {...student, grade: sgrade ? sgrade.grade : 'N/A'};
     });
 }
