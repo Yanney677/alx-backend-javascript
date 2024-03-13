03d32cce1c1ee47744b6b9d12fc1e1fc987aa95a
Yanney677 <w.yanney1991@gmail.com>
2024-03-13 03:15:25 +0000

    Array

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,24 +1,7 @@
-/* A function that returns an array of students for a specific
- * city with their new grade */
-export default function updateStudentGradeByCity(studentlist, city, newGrade) {
-  // Filter student by the specified city
-  return studentlist
-    .filter((obj) => obj.location === city)
-    .map((student) => {
-    // Find the grade object corresponding to the student
-      newGrade.map((studentGrade) => {
-        if (studentGrade.studentId === student.id) {
-          student.grade = studentGrade.grade;
-        }
-
-    /* If gradeObj is found, return the student object with
-     the updated grade, otherwise set grade */
-        if (!student.hasOwnProperty('grade')) {
-          student.grade = 'N/A';
-        }
-        return student;
-      });
-
-      return student;
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter(student => student.location === city)
+    .map(student => {
+      const studgrade = newGrades.find(grade => grade.studentId === student.id);
+      return {...student, grade: studgrade ? studgrade.grade : 'N/A'};
     });
 }
