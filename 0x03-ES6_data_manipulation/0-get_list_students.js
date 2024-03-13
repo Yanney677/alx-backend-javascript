589d2bbe4175532bda5bb1285a01e4b51a1f09d4
Yanney677 <w.yanney1991@gmail.com>
2024-03-13 03:05:13 +0000

    Array

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,9 @@
-/**
- * Retrieves a list of students.
- * @returns {{id: Number, firstName: String, location: String}[]}
- */
 export default function getListStudents() {
   const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
+
   return students;
 }
